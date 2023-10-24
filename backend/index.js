e07aae426dd7574d4ad94d74e6b9b01a840f4c3f
trondaal <trondaal@ntnu.no>
2023-10-24 13:31:08 +0200

    testing new syntax of getting identifier

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // noinspection GraphQLMissingType
 const typeDefs = `
     type Expression @fulltext(indexes: [{ indexName: "expressions", fields: ["titles", "names"] }]) {
-        _id: Int!
+        id: ID! @id
         label: String
         uri: String
         title: String
@@ -30,7 +30,7 @@ const typeDefs = `
         relatedFrom: [Expression!]! @relationship(type: "RELATED", properties: "roleType", direction: IN)
     }
     type Work @fulltext(indexes: [{ indexName: "works", fields: ["titles", "names"] }]) {
-        _id: Int!
+        id: ID! @id
         label: String
         uri: String
         title: String
@@ -51,7 +51,7 @@ const typeDefs = `
 
     }
     type Manifestation {
-        _id: Int!
+        id: ID! @id
         label: String
         uri: String
         identifier: String
@@ -90,12 +90,12 @@ const typeDefs = `
         expressions: [Expression!]! @relationship(type: "EMBODIES", direction: OUT)
     }
     type Concept{
-        _id: Int!
+        id: ID! @id
         label: String,
         uri: String
     }
     type Agent {
-        _id: Int!
+        id: ID! @id
         label: String
         name: String
         uri: String
