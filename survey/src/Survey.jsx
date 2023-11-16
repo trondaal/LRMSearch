ad8d5eebf4e0a38b7866bbdf96bb5dcd1ace0188
trondaal <trondaal@ntnu.no>
2023-11-16 10:16:16 +0100

    Fixing errors

--- survey/src/Survey.jsx
+++ survey/src/Survey.jsx
@@ -14,27 +14,24 @@ const questions = [
     ["You have been told by a friend to read the story \"Chronopolis\" by J.G. Ballard and have made a search using title and author name.",
         "http://dijon.idi.ntnu.no/lrm-search/?query=chronopolis+ballard",
         "Results for the search on \"Chronopolis Ballard\""],
-    ["You want to explore what is available by Mark Twain and have made a search using the authors name.",
-        "http://dijon.idi.ntnu.no/lrm-search/?query=mark+twain",
-        "Results for the search om \"Mark Twain\""],
     ["You want to explore what is available by Mark Twain on the character Tom Sawyer and have made a search using the authors name and the name of the character.",
         "http://dijon.idi.ntnu.no/lrm-search/?query=mark+twain+tom+sawyer",
-        "Results for the search om \"Mark Twain Tom Sawyer\""],
-    ["You want to read \"Lord of the rings\" by J.R.R. and have made a search using this title.",
+        "Results for the search on \"Mark Twain Tom Sawyer\""],
+    ["You want to read \"Lord of the rings\" by J.R.R. Tolkien and have made a search using this title.",
         "http://dijon.idi.ntnu.no/lrm-search/?query=lord+rings&language=english",
         "Results for the search on \"Lord of the rings\""],
     ["You want to read \"Murder on the links\" by Agatha Christie and have made a search using this title.",
-        "http://dijon.idi.ntnu.no/lrm-search/?query=Murder+on+the+links",
+        "http://dijon.idi.ntnu.no/lrm-search/?query=Murder+on+the+links&sort=random",
         "Results for the search on \"Murder on the links\""],
-    ["You want to read \"Murder on the Orient Express\" by \"Agatha Christie\" and have made a search using title and author name.",
-        "http://localhost:5173/lrm-search/?query=murder+on+the+orient+express+Agatha+Christie&content=text&name=christie&sort=random",
+    ["You want to read \"Murder on the Orient Express\" by Agatha Christie and have made a search using title and author name.",
+        "http://dijon.idi.ntnu.no/lrm-search/?query=murder+on+the+orient+express+Agatha+Christie&content=text&name=christie&sort=random",
         "Results for the search on \"Murder on the Orient Express Agatha Christie\""],
 
 ];
 const tasks = [
-    "Marker as relevant the result entries that you most immediately find interesting to inspect. Please mark at least one.",
-    "Mark as relevant the entries that you find to best match the query and mark as irrelevant the entries that you find to have least match. Mark at least one as relevant and one as irrelevant.",
-    "Mark as relevant the entries that you think of as distinct, individual content, and mark as irrelevant the entries that in your opinion least represent a single piece of content.",
+    "Mark as relevant the result(s) that you most immediately find interesting to inspect. Please mark at least one.",
+    "Mark as relevant the entries that you find to best match the query and mark as non-relevant the entries that you find to have least match. Mark at least one as relevant and one as irrelevant.",
+    "Mark as relevant all entries that you think of as works by this author",
 ]
 
 function getRandomQuestions(questions, num) {
@@ -83,7 +80,7 @@ const Survey = () => {
                 On the top of the page is a button that can be used to hide and show all available publications.</p>
             <p>To the left of each entry on the result page, there are two circular buttons with up and down arrows that can be used to
                 indicate how you assess entries according to the task given. The up arrow button <ArrowCircleUpIcon color="action" fontSize="small" style={{ verticalAlign: 'middle' }}/> is used to give positive feedback on an entry, the down arrow button <ArrowCircleDownIcon color="action" fontSize="small" style={{ verticalAlign: 'middle' }}/> to give negative feedback.
-                After marking, entries with positive feedback are highlighted in green or highlighted with red when negative negative. Markings of a particular entry can be removed clicking on the cancel button <RemoveCircleOutlineIcon color="action" fontSize="small" style={{ verticalAlign: 'middle' }}/>.
+                After marking, entries with positive feedback are highlighted in green or highlighted with red when negative. Markings of a particular entry can be removed clicking on the cancel button <RemoveCircleOutlineIcon color="action" fontSize="small" style={{ verticalAlign: 'middle' }}/>.
                 When you decide to finish a task, click on the "Submit" button to send your feedback. This will show a dialog where you can give some supplemental feedback before pressing Yes to finish the submission. Screenshots of the application and the submit dialogue are shown below.
             </p>
             <img src={screenshots} alt={"Screenshot of resultlisting and submission dialogue"} style={{ width: '80%'}} align="middle"/>
@@ -94,7 +91,7 @@ const Survey = () => {
                 <div key={index}>
                     <h2>Task {index + 1}</h2>
                     <p className={"section"}><span className={"prefix"}>Context: </span><span className={"description"}>{question[0]}</span></p>
-                    <p className={"section"}><span className={"prefix"}>Link: </span><span className={"description"}><a href={question[1] + "&respondent=" + respondent + "&task=" + (index+1)} target={"_blank"} rel="noreferrer">{question[2]}</a></span></p>
+                    <p className={"section"}><span className={"prefix"}>Link: </span><span className={"description"}><a href={question[1] + "&respondent=" + respondent + "&task=" + (index+1) + "&description=" + tasks[index]} target={"_blank"} rel="noreferrer">{question[2]}</a></span></p>
                     <p className={"section"}><span className={"prefix"}>Task: </span><span className={"description"}>{tasks[index]}</span></p>
                 </div>
             ))}
